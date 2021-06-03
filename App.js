4828079f81947007b6cde33b9426fa370814e838
jvsinghk <jvsinghk@gmail.com>
2021-06-03 20:35:31 +0530

    Add scrollview to account for divice diffrences and update placholders

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import LabelInput from './components/LabelInput';
 import PasswordInput from './components/PasswordInput';
 
@@ -7,57 +7,62 @@ const App = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [retypePassword, setRetypePassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
   const [petName, setPetName] = useState('');
   const [petDob, setPetDob] = useState('');
   const [breed, setBreed] = useState('');
   const [favoriteToy, setFavoriteToy] = useState('');
   
 return (
-  <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1', padding: 16 }}>
+  <ScrollView 
+    style={{ 
+      flex: 1, 
+      backgroundColor: '#ecf0f1', 
+      padding: 16 
+    }}>
     <LabelInput 
       label='Email' 
-      placeholder='Enter email'
+      placeholder='Type your email here'
       value={email}
       handleChange={setEmail} 
     />
     <PasswordInput
      label='Password'
-     placeholder='Enter password'
+     placeholder='Type your password here'
      value={password}
      handleChange={setPassword}
     />
     <PasswordInput
-     label='Retype Password'
+     label='Confirm Password'
      placeholder='Enter password'
-     value={retypePassword}
-     handleChange={setRetypePassword}
+     value={confirmPassword}
+     handleChange={setConfirmPassword}
     /> 
     <LabelInput
-      label={`Pet's Name`}
-      placeholder={`Enter pet's name`}
+      label='Name'
+      placeholder={`Type your pet's name here`}
       value={petName}
       handleChange={setPetName}
     /> 
     <LabelInput
-      label={`Pet's Date of Birth`}
-      placeholder={`Enter pet's date of birth`}
+      label='Bithday'
+      placeholder={`Type your pet's date of birth here`}
       value={petDob}
       handleChange={setPetDob} 
     />
     <LabelInput 
       label='Breed'
-      placeholder='Enter Breed'
+      placeholder={`Type your pet's breed here`}
       value={breed}
       handleChange={setBreed}
     />
     <LabelInput
       label='Favorite Toy'
-      placeholder='Enter favorite toy'
+      placeholder={`Type your pet's favorite toy here`}
       value={favoriteToy}
       handleChange={setFavoriteToy}
     />
-  </View>
+  </ScrollView>
 )};
 
 export default App;
