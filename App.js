a72529f801dca1bc223140d563e0eb08e6f757ff
jvsinghk <jvsinghk@gmail.com>
2021-06-03 19:34:30 +0530

    Add sign up form fields using custom component and tie the inut from each of those custom component back to root component using useState

--- App.js
+++ App.js
@@ -1,9 +1,55 @@
-import React from 'react';
-import { View, Text, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View } from 'react-native';
+import LabelInput from './components/LabelInput';
 
-const App = () => (
+const App = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [petName, setPetName] = useState('');
+  const [petDob, setPetDob] = useState('');
+  const [breed, setBreed] = useState('');
+  const [favoriteToy, setFavoriteToy] = useState('');
+  
+return (
   <View style={{ flex: 1, justifyContent: 'center', backgroundColor: '#ecf0f1' }}>
+    <LabelInput 
+      label='Email' 
+      placeholder='Enter email'
+      value={email}
+      handleChange={setEmail} 
+    />
+    <LabelInput
+     label='Password'
+     placeholder='Enter password'
+     value={password}
+     handleChange={setPassword}
+    /> 
+    <LabelInput
+      label={`Pet's Name`}
+      placeholder={`Enter pet's name`}
+      value={petName}
+      handleChange={setPetName}
+    /> 
+    <LabelInput
+      label={`Pet's Date of Birth`}
+      placeholder={`Enter pet's date of birth`}
+      value={petDob}
+      handleChange={setPetDob} 
+    />
+    <LabelInput 
+      label='Breed'
+      placeholder='Enter Breed'
+      value={breed}
+      handleChange={setBreed}
+    />
+    <LabelInput
+      label='Favorite Toy'
+      placeholder='Enter favorite toy'
+      value={favoriteToy}
+      handleChange={setFavoriteToy}
+    />
   </View>
-);
+)};
 
 export default App;
